08e12b39af78faa23984d6dcb56a58fdc147f699
MrHecka <muhammadmamad41@gmail.com>
2022-05-17 22:21:53 +0700

    fix db

--- botspam.py
+++ botspam.py
@@ -31,9 +31,7 @@ app = Flask(__name__)
 def hello():
     return "Halo Ngab Ada Apa Yaa??"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+
 
 
 
@@ -732,6 +730,9 @@ updater.dispatcher.add_handler(CommandHandler('dels', dels))
 updater.dispatcher.add_handler(CommandHandler('list', list))
 
   
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 print("BOT BERJALAN....")
